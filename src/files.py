73fecfcc884f98a628cc37a65e18af382977e8fd
Toni Heittola <toni.heittola@gmail.com>
2017-05-09 18:30:27 +0300

    Use soundfile package to read wav audio

--- src/files.py
+++ src/files.py
@@ -8,7 +8,7 @@ import csv
 import cPickle as pickle
 import librosa
 import yaml
-
+import soundfile
 
 def load_audio(filename, mono=True, fs=44100):
     """Load audio file into numpy array
@@ -40,44 +40,13 @@ def load_audio(filename, mono=True, fs=44100):
 
     file_base, file_extension = os.path.splitext(filename)
     if file_extension == '.wav':
-        audio_file = wave.open(filename)
-
-        # Audio info
-        sample_rate = audio_file.getframerate()
-        sample_width = audio_file.getsampwidth()
-        number_of_channels = audio_file.getnchannels()
-        number_of_frames = audio_file.getnframes()
-
-        # Read raw bytes
-        data = audio_file.readframes(number_of_frames)
-        audio_file.close()
-
-        # Convert bytes based on sample_width
-        num_samples, remainder = divmod(len(data), sample_width * number_of_channels)
-        if remainder > 0:
-            raise ValueError('The length of data is not a multiple of sample size * number of channels.')
-        if sample_width > 4:
-            raise ValueError('Sample size cannot be bigger than 4 bytes.')
-
-        if sample_width == 3:
-            # 24 bit audio
-            a = numpy.empty((num_samples, number_of_channels, 4), dtype=numpy.uint8)
-            raw_bytes = numpy.fromstring(data, dtype=numpy.uint8)
-            a[:, :, :sample_width] = raw_bytes.reshape(-1, number_of_channels, sample_width)
-            a[:, :, sample_width:] = (a[:, :, sample_width - 1:sample_width] >> 7) * 255
-            audio_data = a.view('<i4').reshape(a.shape[:-1]).T
-        else:
-            # 8 bit samples are stored as unsigned ints; others as signed ints.
-            dt_char = 'u' if sample_width == 1 else 'i'
-            a = numpy.fromstring(data, dtype='<%s%d' % (dt_char, sample_width))
-            audio_data = a.reshape(-1, number_of_channels).T
+        # Load audio
+        audio_data, sample_rate = soundfile.read(filename)
+        audio_data = audio_data.T
 
         if mono:
             # Down-mix audio
             audio_data = numpy.mean(audio_data, axis=0)
-
-        # Convert int values into float
-        audio_data = audio_data / float(2 ** (sample_width * 8 - 1) + 1)
 
         # Resample
         if fs != sample_rate:
